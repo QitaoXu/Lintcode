4e394817c79bd6280e254707ba3a466c2fc32c80
Qitao Xu <jeremyxuqitao@outlook.com>
2019-04-03 11:18:45 -0500

    Reset User

--- interviews/P/containDupIII.py
+++ interviews/P/containDupIII.py
@@ -6,22 +6,38 @@ class Solution:
     @return: whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
     """
     def containsNearbyAlmostDuplicate(self, nums, k, t):
-        # Write your code here
         
-        hash_set = set()
+        if t < 0:
+            
+            return False 
+        
+        bucket_to_num = {}
+        
+        w = t + 1 
         
         for i in range(len(nums)):
             
-            for diff in range(0, t + 1):
-                
-                if nums[i] - diff in hash_set or nums[i] + diff in hash_set:
-                    
-                    return True 
-                    
-            hash_set.add(nums[i])
+            m = self.get_ID(nums[i], w)
             
-            if len(hash_set) > k:
+            if m in bucket_to_num:
                 
-                hash_set.remove(nums[i - k])
+                return True 
+            
+            if (m + 1) in bucket_to_num and abs(bucket_to_num[m + 1] - nums[i]) < w:
+                return True  
+            
+            if (m - 1) in bucket_to_num and abs(bucket_to_num[m - 1] - nums[i]) < w:
+                return True 
+            
+            bucket_to_num[m] = nums[i]
+            
+            if i >= k:
+                del bucket_to_num[self.get_ID(nums[i - k], w)]
                 
         return False 
+        
+    def get_ID(self, x, w):
+        
+        return  x // w
+        # java 
+        # return x < 0 ? (x + 1) / w - 1 : x / w
